f05d9b9710ab46574e89ccff4f73921cb208ea41
chasem <cpmcquown@gmail.com>
2021-06-15 13:46:57 -0700

    added fetch request

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,35 @@
 // identify search button
 var searchBtn = document.querySelector("#search-btn");
 
-// store the city that the user is searching for in a variable
+// identify input element
 var usersCity = document.querySelector("#city");
 
 // this is where the forecast will be generated and displayed to webpage
-var generateForecast = function() {
+var getCity = function() {
+
+    // store the city that the user is searching for in a variable
     var city = usersCity.value.trim();
-    alert("You searched for " + city); 
+
+    // api key 
+    var apiKey = "&appid=cf6215d35458f7e05133781f893bec16";
+
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey
+    ).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+    
+    // dynamically generate button and add to card that will have inner html = the city that the user searched for
+
+    // dynamically generate a container to the right of card that will display cities name and date, current temp below that, wind speed below that, humifity below that, and uv index below that
+
+    // dynamically generate a new container that has 5 cards displayed horizontally. The cards will contain date, temp, wind, and humidity
+
 };
 
 
-// listen for click on search button, then do something--generateForecast()--once button is clicked.
-searchBtn.addEventListener("click", generateForecast);
+// listen for click on search button, then do something--getCity()--once button is clicked.
+searchBtn.addEventListener("click", getCity);
